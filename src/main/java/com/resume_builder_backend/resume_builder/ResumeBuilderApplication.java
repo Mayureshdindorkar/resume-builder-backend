b690b46d0f9c40f0c4307571aa7270c87fba617a
Mayureshdindorkar <dmayuresh99@gmail.com>
2024-07-31 18:34:32 +0530

    CORS fixed for PATCH

--- src/main/java/com/resume_builder_backend/resume_builder/ResumeBuilderApplication.java
+++ src/main/java/com/resume_builder_backend/resume_builder/ResumeBuilderApplication.java
@@ -18,7 +18,10 @@ public class ResumeBuilderApplication {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("*");
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                        .allowedHeaders("*");
             }
         };
     }
